ee722d181433205ba479f7e000dee9fc2518c84a
Lucas Correa <lucas.dcorrea1@gmail.com>
2020-01-30 16:27:17 -0300

    fix test cors

--- src/app.js
+++ src/app.js
@@ -23,9 +23,10 @@ const slideshowRoutes = require('./Routes/slideshowRouts');
 const authRoutes = require('./Routes/authUserRoutes');
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  var origin = req.get('origin'); 
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
