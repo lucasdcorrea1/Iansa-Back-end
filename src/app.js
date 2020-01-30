1fcd00b13ca2cd9b07d452171300fecda82b0d5e
Lucas Correa <lucas.dcorrea1@gmail.com>
2020-01-30 15:58:36 -0300

    fix test cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 //Carregando as Models
@@ -23,9 +23,8 @@ const slideshowRoutes = require('./Routes/slideshowRouts');
 const authRoutes = require('./Routes/authUserRoutes');
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
