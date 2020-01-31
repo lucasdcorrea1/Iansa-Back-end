5a9e2901915750ad9f7f1b75a750cb221ceda5ff
Lucas Correa <lucas.dcorrea1@gmail.com>
2020-01-31 01:44:53 -0300

    format data get trasparency

--- src/App/Controllers/Transparency/transparency.js
+++ src/App/Controllers/Transparency/transparency.js
@@ -5,8 +5,22 @@ module.exports = {
   async getImage(req, res) {
     try {
       const posts = await transparencyRepository.getAll();
+      var formatedPost = []
 
-      return res.status(200).json(posts);
+      if(posts){
+        posts.forEach(item => {
+          formatedPost.push({
+            _id: item._id,
+            title: item.title,
+            url: item.url,
+            description: item.description,
+            name: item.name,
+            createdAt: `${item.createdAt.getDate()+1}/${item.createdAt.getMonth()+1}/${item.createdAt.getFullYear()}`
+          });
+        });
+      };
+      
+      return res.status(200).json(formatedPost);
     } catch (error) {
       res.status(400).send({
         error: `Erro ao realizar busca: ${error}`
